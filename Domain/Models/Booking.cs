6c47568dc94756a5bbc5f0f31380e93f85fa8415
Ratna Bhindi <ratna.bhindi@gmail.com>
2024-04-30 15:51:23 +1000

    Add Booking model

---
+++ Domain/Models/Booking.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class Booking
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        public DateTime BookingTime { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 3)]
+        public string Name { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime DateCreated { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime DateModified { get; set; }
+
+        // Constructor to initialize DateCreated and DateModified
+        public Booking()
+        {
+            DateCreated = DateTime.UtcNow;
+            DateModified = DateTime.UtcNow;
+        }
+    }
+}
